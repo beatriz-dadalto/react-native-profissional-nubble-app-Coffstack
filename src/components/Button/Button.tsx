f70de1cb50b39481ec0fff7160ac9844fb25ec7e
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-05 15:25:18 -0300

    Box Component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,25 +3,18 @@ import {TouchableOpacity} from 'react-native';
 import {Text} from '../Text/Text';
 import {useTheme} from '@shopify/restyle';
 import {Theme} from '../../theme/theme';
+import {Box} from '../Box/Box';
 
 interface ButtonProps {
   title: string;
 }
 
 export function Button({title}: ButtonProps) {
-  const {colors} = useTheme<Theme>();
   return (
-    <TouchableOpacity
-      style={{
-        paddingHorizontal: 20,
-        paddingVertical: 14,
-        backgroundColor: colors.carrotSecondary,
-        borderRadius: 16,
-        alignItems: 'center',
-      }}>
+    <Box backgroundColor="buttonPrimary" paddingHorizontal="s56">
       <Text preset="paragraphMedium" bold style={{color: '#FFF'}}>
         {title}
       </Text>
-    </TouchableOpacity>
+    </Box>
   );
 }
