ba4a7dd07d7ea7052b110f9de75711b7f3316455
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-05 15:08:51 -0300

    Restyle - Criando o tema na App

--- App.tsx
+++ App.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {Button} from './src/components/Button/Button';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <View style={{paddingHorizontal: 24}}>
-        <Text preset="headingLarge" bold>
-          Beatriz Dadalto!
-        </Text>
-        <Text preset="headingSmall" italic>
-          Beatriz Dadalto!
-        </Text>
-        <Button title="Entrar" />
-      </View>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge" bold>
+            Beatriz Dadalto!
+          </Text>
+          <Text preset="headingSmall" italic>
+            Beatriz Dadalto!
+          </Text>
+          <Button title="Entrar" />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
