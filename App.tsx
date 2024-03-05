2d5546adeaaba75fe25a1729fd16e3819c12af60
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-05 14:35:08 -0300

    Como não fazer o botão

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
+import {Button} from './src/components/Button/Button';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <Text preset="headingLarge" bold>
-        Beatriz Dadalto!
-      </Text>
-      <Text preset="headingSmall" italic>
-        Beatriz Dadalto!
-      </Text>
+      <View style={{paddingHorizontal: 24}}>
+        <Text preset="headingLarge" bold>
+          Beatriz Dadalto!
+        </Text>
+        <Text preset="headingSmall" italic>
+          Beatriz Dadalto!
+        </Text>
+        <Button title="Entrar" />
+      </View>
     </SafeAreaView>
   );
 }
