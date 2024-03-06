7e1efd8bbf562ba40495335c6e8616a212504d0c
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-06 15:49:55 -0300

    TextInput - introdução

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, TextInput, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Icon} from './src/components/Icon/Icon';
+import {Box} from './src/components/Box/Box';
+import {Button} from './src/components/Button/Button';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text preset="headingLarge" bold>
-            Beatriz Dadalto!
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá, Beatriz Dadalto!
           </Text>
-          <Icon name="eyeOn" color="error" size={40} />
-          <Icon name="eyeOff" color="carrotSecondaryLight" size={90} />
+          <Text preset="paragraphLarge" marginBottom="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <Box marginBottom="s20">
+            <TextInput
+              placeholder="Digite seu e-mail"
+              style={{borderWidth: 1, height: 50}}
+            />
+          </Box>
+          <Box>
+            <TextInput
+              placeholder="Digite sua senha"
+              style={{borderWidth: 1, height: 50}}
+            />
+          </Box>
+          <Text marginTop="s8" color="primary" preset="paragraphSmall" bold>
+            Esqueci minha senha
+          </Text>
+          <Button title="Entrar" preset="primary" marginTop="s48" />
+          <Button title="Criar uma conta" preset="outline" marginTop="s12" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
