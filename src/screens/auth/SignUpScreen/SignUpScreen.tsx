2472661e4fb65a7c4aba57dbd8ebd42f1717448b
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-08 18:16:40 -0300

    ScreenComponent - SignUpScreen e Botão de Voltar

---
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text/Text';
+import {TextInput} from '../../../components/TextInput/TextInput';
+import {Icon} from '../../../components/Icon/Icon';
+import {Button} from '../../../components/Button/Button';
+
+export function SignUpScreen() {
+  function submitForm() {
+    // TODO: implementar
+  }
+
+  return (
+    <Screen canGoBack>
+      <Text preset="headingLarge" marginBottom="s32">
+        Criar uma conta
+      </Text>
+      <TextInput
+        label="Seu username"
+        placeholder="@"
+        boxProps={{marginBottom: 's16'}}
+      />
+      <TextInput
+        label="Nome Completo"
+        placeholder="Digite seu nome completo"
+        boxProps={{marginBottom: 's16'}}
+      />
+      <TextInput
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{marginBottom: 's16'}}
+      />
+      <TextInput
+        label="Senha"
+        placeholder="Digite sua senha"
+        RightComponent={<Icon color="gray2" name="eyeOn" />}
+        boxProps={{marginBottom: 's48'}}
+      />
+      <Button title="Criar uma conta" onPress={submitForm} />
+    </Screen>
+  );
+}
