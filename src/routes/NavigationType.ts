fb9ac59ba535d12052f1a10d3b81e0bc67fab36d
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-14 12:11:20 -0300

    Módulo Navegação Avançada: Refatoração - Auth Screens e RootParamList

--- src/routes/NavigationType.ts
+++ src/routes/NavigationType.ts
@@ -1,14 +1,16 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-import {RootStackParamList} from '@routes';
-
 import {AppStackParamList} from './AppStack';
+import {AuthStackParamList} from './AuthStack';
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
+    interface RootParamList extends AuthStackParamList, AppStackParamList {}
   }
 }
 
 export type AppScreenProps<TRouteName extends keyof AppStackParamList> =
   NativeStackScreenProps<AppStackParamList, TRouteName>;
+
+export type AuthScreenProps<TRouteName extends keyof AuthStackParamList> =
+  NativeStackScreenProps<AuthStackParamList, TRouteName>;
