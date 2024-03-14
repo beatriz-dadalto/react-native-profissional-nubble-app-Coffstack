17ab0504c1bd877b7de71680d89ba6d8b90ee768
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-14 11:08:06 -0300

    Módulo Navegação Avançada: Usando TypeScript Generics no Type das Screens

--- src/routes/NavigationType.ts
+++ src/routes/NavigationType.ts
@@ -1,7 +1,14 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+
 import {RootStackParamList} from '@routes';
+
+import {AppStackParamList} from './AppStack';
 
 declare global {
   namespace ReactNavigation {
     interface RootParamList extends RootStackParamList {}
   }
 }
+
+export type AppScreenProps<TRouteName extends keyof AppStackParamList> =
+  NativeStackScreenProps<AppStackParamList, TRouteName>;
