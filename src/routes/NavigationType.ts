2f435055710a9ddf09a689a711fc9fa31aba66f5
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-14 14:37:41 -0300

    Módulo Navegação Avançada: Navegação e TypeScript com Nested Navigator

--- src/routes/NavigationType.ts
+++ src/routes/NavigationType.ts
@@ -1,6 +1,9 @@
+import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
+import {CompositeScreenProps} from '@react-navigation/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import {AppStackParamList} from './AppStack';
+import {AppTabBottomTabParamList} from './AppTabNavigator';
 import {AuthStackParamList} from './AuthStack';
 
 declare global {
@@ -14,3 +17,10 @@ export type AppScreenProps<TRouteName extends keyof AppStackParamList> =
 
 export type AuthScreenProps<TRouteName extends keyof AuthStackParamList> =
   NativeStackScreenProps<AuthStackParamList, TRouteName>;
+
+export type AppTabScreenProps<
+  TRouteName extends keyof AppTabBottomTabParamList,
+> = CompositeScreenProps<
+  BottomTabScreenProps<AppTabBottomTabParamList, TRouteName>,
+  NativeStackScreenProps<AppStackParamList, 'AppTabNavigator'>
+>;
