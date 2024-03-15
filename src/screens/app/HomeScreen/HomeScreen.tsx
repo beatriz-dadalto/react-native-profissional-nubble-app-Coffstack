56321850af3c8d035e349fa58816c21afb26192f
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-15 13:37:02 -0300

    Módulo Componentes Complexos: Integrando Service na Home Screen

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
-import {Button, Screen, Text} from '@components';
+import {Post, postService} from '@domain';
+
+import {Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
+  const [postList, setPostList] = React.useState<Post[]>();
+
+  React.useEffect(() => {
+    postService.getList().then(itemList => setPostList(itemList));
+  }, []);
+
   return (
     <Screen>
-      <Text preset="headingLarge">Home Screen, Bia</Text>
-      <Button
-        title="Settings"
-        onPress={() => navigation.navigate('SettingsScreen')}
-      />
-      <Button
-        title="Favorite"
-        marginTop="s12"
-        onPress={() => navigation.navigate('FavoriteScreen')}
-      />
+      {postList?.map(post => (
+        <Text key={post.id}>{post.author.name}</Text>
+      ))}
     </Screen>
   );
 }
