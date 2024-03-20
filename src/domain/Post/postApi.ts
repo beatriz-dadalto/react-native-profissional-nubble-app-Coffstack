c620551e79d3f2f6e32764878513a7b586436709
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-20 16:53:03 -0300

    Módulo Arquitetura para Frontend: Camada de API

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,9 +1,18 @@
-import {postListMock} from './postListMock';
-import {Post} from './types';
+import {PageAPI} from '@api';
 
-async function getList(): Promise<Post[]> {
-  await new Promise(resolve => setTimeout(() => resolve(''), 2000));
-  return postListMock;
+import {PostAPI} from './postTypes';
+
+async function getList(): Promise<PageAPI<PostAPI>> {
+  let response = await fetch('http://127.0.0.1:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer NA.bNF6_k-I-NLGteuhxXjyaBxd1tYP3TmKIFn2Y6Z4mkgBi4AVMvdSd_PGvlOR',
+    },
+  });
+
+  let data: PageAPI<PostAPI> = await response.json();
+  return data;
 }
 
 export const postApi = {
---
+++ src/domain/Post/postTypes.ts
@@ -0,0 +1,41 @@
+export interface Post {
+  id: string;
+  text: string;
+  author: {
+    profileURL: string;
+    name: string;
+    userName: string;
+  };
+  imageURL: string;
+  reactionCount: number;
+  commentCount: number;
+  favoriteCount: number;
+}
+
+// REPRESENTAÇÃO DOS DADOS DE COMO ESTÃO NA API
+export interface PostAPI {
+  id: number; // 1
+  text: string; //'Bom dia!'
+  user_id: number; // 1
+  image_url: string; // 'https://nubble-development.s3.sa-east-1.amazonaws.com/backend-integration/post1.jpg'
+  is_fixed: boolean; // false
+  is_activated: boolean; // true
+  created_at: string; // '2024-03-18T18:01:08.783-03:00'
+  updated_at: string; // '2024-03-18T18:01:08.789-03:00'
+  user: {
+    id: number; // 1
+    first_name: string; // 'Maria'
+    last_name: string; // 'Julia'
+    username: string; // 'mariajulia'
+    email: string; // 'mariajulia@coffstack.com'
+    profile_url: string; // 'https://nubble-development.s3.sa-east-1.amazonaws.com/backend-integration/1-maria.png'
+    is_online: boolean; // false
+    full_name: string; // 'Maria Julia'
+  };
+  status: string; // 'published'
+  meta: {
+    like_count: string; // '7'
+    favorite_count: string; // '4'
+    comments_count: string; // '2'
+  };
+}
