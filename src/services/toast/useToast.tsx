41a0be3c677c7447804000a4e4f1aece64eb32f4
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-04-06 12:54:16 -0300

    Módulo Estado Global com Context e Zustand: Estado Global com Zustand

--- src/services/toast/useToast.tsx
+++ src/services/toast/useToast.tsx
@@ -1,7 +1,29 @@
 import {ToastService} from './toastTypes';
-import {useToastContext} from './useToastContext';
+import {useToastServiceZustand, useToastZustand} from './useToastZustand';
 
-/**@description Isola a implementação de `useToastContext()` para poder alternar facilmente entre context e zustand ou algum outro meio de genrência de estado */
-export function useToast(): ToastService {
-  return useToastContext();
+/**
+ * @description Isola a implementação de um gerenciador de estado para poder alternar facilmente entre context e zustand ou algum outro meio de genrência de estado
+ * @returns retorne `useToastContext()` ou `useToastZustand()` dependendo de qual o projeto está usando.
+ */
+export function useToast(): ToastService['toast'] {
+  // usando o context
+  // const {toast} = useToastContext();
+  // return toast;
+  const toast = useToastZustand();
+  return toast;
+}
+
+export function useToastService(): Pick<
+  ToastService,
+  'showToast' | 'hideToast'
+> {
+  // usando o context
+  // const {showToast, hideToast} = useToastContext();
+
+  // return {
+  //   showToast,
+  //   hideToast,
+  // };
+
+  return useToastServiceZustand();
 }
