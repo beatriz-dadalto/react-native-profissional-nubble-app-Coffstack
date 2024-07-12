99a3c80c400e71fedafa5784bac67d96a947035d
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-07-12 08:25:45 -0300

    Módulo Estado Global assíncrono com TanStack Query: Cache e Stale Time

--- src/domain/User/useCases/useUserGetById.ts
+++ src/domain/User/useCases/useUserGetById.ts
@@ -7,6 +7,8 @@ export function useUserGetById(id: number) {
   const {data, isLoading, isError} = useQuery({
     queryKey: [QueryKeys.UserGetById, id],
     queryFn: () => userService.getById(id),
+    staleTime: 1000 * 10, // 10 seconds to query turn obsolete
+    // cacheTime: 5000, // 5 seconds to clean cache
   });
 
   return {
